e8066f3024c7a72233d27dc66f6c8b2942b49933
RJ-Batiller <robertjoshua4444@gmail.com>
2025-11-20 18:29:19 +0800

    Added some functions

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import time
+
 from datetime import datetime
 
 def load_data(filename):
@@ -9,4 +9,30 @@ def load_data(filename):
         return{}
     
 def save_data(filename,data):
-    with open(filename, )
+    with open(filename, "w") as file:
+        file.write(str(data))
+
+employees_file = "employee.txt"
+attendance_file = "attendance.txt"
+payroll_file = "payroll.txt"
+
+employees = load_data(employees_file)
+attendance = load_data(attendance_file)
+payroll = load_data(payroll_file)
+
+def add_employee(emp_id, name, role, dept, rate, contact):
+    employees(emp_id) = {
+        "name": name,
+        "role": role,
+        "dept": dept,
+        "role": role,
+        "rate": float(rate),
+        "contact": contact
+    }
+    save_data(employees_file, employees)
+    print("✅Employees Successfully Added")
+
+
+load_data()
+save_data()
+add_employee()
